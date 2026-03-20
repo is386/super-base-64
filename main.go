ae4920b07a2ccfaa4d135ae33abd41886cb4518f
is386 <20195347+is386@users.noreply.github.com>
2026-03-19 23:39:58 -0400

    add encoding algorithm

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+const table = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
+func main() {
+	input := "And"
+	bytesData := []byte(input)
+
+	fmt.Println("Original:", input)
+
+	var encoded strings.Builder
+	var missing int
+	for i := 0; i < len(bytesData); i += 3 {
+		missing = 0
+
+		var byteSlice []byte
+		if i+3 > len(bytesData) {
+			missing = i + 3 - len(bytesData)
+			byteSlice = bytesData[i:]
+		} else {
+			byteSlice = bytesData[i : i+3]
+		}
+
+		var bits uint32
+		for _, b := range byteSlice {
+			bits = uint32(b) | bits<<8
+		}
+		if missing != 0 {
+			bits <<= (8 * (missing))
+		}
+
+		for j := 3; j >= missing; j-- {
+			sixBitGroup := (bits & (0b111111 << (j * 6))) >> (j * 6)
+			fmt.Fprintf(&encoded, "%c", table[sixBitGroup])
+		}
+	}
+
+	for i := 0; i != missing; i++ {
+		fmt.Fprintf(&encoded, "%s", "=")
+	}
+
+	fmt.Println("Encoded: ", encoded.String())
+}
